072c08ef9562d24ad9db3faaf0957c36a7861151
Xavier.L <xavierlee0329@gmail.com>
2025-08-29 17:57:44 +0800

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,86 @@
-import { useEffect, useState } from "react";
-import countryInfo from "./services/country";
-import FilterCountries from "./components/FilterCountries";
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import noteService from './services/notes'
+import Footer from './components/Footer'
 
 const App = () => {
-  const [country, setCountry] = useState("");
-  const [countries, setCountries] = useState([]);
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    countryInfo.getAll().then((countries) => {
-      setCountries(countries);
-    });
-  }, []);
+    noteService.getAll().then((initialNotes) => {
+      console.log('API return: ', initialNotes);
+      setNotes(initialNotes)
+    })
+  }, [])
 
-  const filterCountries = countries.filter((c) =>
-    c.name.common.toLowerCase().includes(country.toLowerCase())
-  );
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
 
-  const showButton = (countryName) => {
-    setCountry(countryName);
-  };
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
+  }
 
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      })
+      .catch(() => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
-        find countries{" "}
-        <input value={country} onChange={(e) => setCountry(e.target.value)} />
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
       </div>
-      <FilterCountries filterCountries={filterCountries} showButton={showButton} />
-      
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
