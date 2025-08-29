ad7a3bccc436c0f3382790ba3732cbc0122b651a
Xavier.L <xavierlee0329@gmail.com>
2025-08-29 18:00:08 +0800

    correct

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://learning-nodejs-hb3u.onrender.com/',
         changeOrigin: true,
       },
     }
